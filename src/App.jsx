11e92c7efe7aa57d46a7d8686bcfaaf757088640
ryowakitazawa82 <ryowa.kitazawa@gmail.com>
2021-10-24 21:25:59 +0900

    renew

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,25 @@
-import styled from 'styled-components';
-import { useState } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { List } from "./List";
 import { Form } from "./Form";
-import { getLanguages } from "./const/languages";
-import { withLoading } from "./hoc/withLoading";
+// import { getLanguages } from "./const/languages";
+// import { withLoading } from "./hoc/withLoading";
 import { Modal } from "./components/modal";
+import { Header } from "./Header";
+import { ThemeContext } from "./contexts/ThemeContext";
+import styled from 'styled-components';
 
-const Header = styled.header`
-display: flex;
-justify-content: space-between;
-padding: 24px 64px 0;
-border-bottom: 1px solid #E0E0E0;
+const  Container = styled.div`
+  height: 100%;
+  color:${({ theme }) => theme.color};
+  background-color: ${({ theme }) => theme.backgroundColor};
 `
 
-const HeaderUl = styled.ul`
-display: flex;
-margin: 0;
-padding: 0;
-`
-
-const HeaderLi = styled.li`
-list-style: none;
-padding: 4px 12px;
-cursor: pointer;
-border-bottom: ${props => props.forcused ? '2px solid #F44336' : 'none'};
-`
 
 function App({ data }) {
   const [tab, setTab] = useState('list');
   const [langs, setLangs] = useState(data);
+
+  const [theme] = useContext(ThemeContext);
 
   const addLang = (lang) => {
     setLangs([...langs, lang]);
@@ -36,19 +27,13 @@ function App({ data }) {
   };
 
   return (
-    <div>
-      <Header>
-        <HeaderUl>
-          <HeaderLi forcused={tab === 'list'} onClick={() => setTab('list')}>リスト</HeaderLi>
-          <HeaderLi forcused={tab === 'form'} onClick={() => setTab('form')}>フォーム</HeaderLi>
-        </HeaderUl>
-      </Header>
+    <Container theme={theme}>
+      <Header tab={tab} setTab={setTab}/>
       {
-
         tab === 'list' ? <List langs={langs}/>: <Form onAddLang={addLang}/>
       }
-    </div>
+    </Container>
   );
 }
 
-export default withLoading(App, getLanguages);
+export default App;
--- src/Form.jsx
+++ src/Form.jsx
@@ -3,6 +3,7 @@ import { Button } from './components/button';
 import styled from 'styled-components';
 import { TabBodyContainer } from './components/tab-body-container ';
 import { FormModal } from './formModal';
+import { Hint } from './Hint';
 
 
 const Label = styled.label`
@@ -44,6 +45,7 @@ export const Form = ({ onAddLang }) => {
                 value={text}
                 onChange={(e) => setText(e.target.value)}
                 />
+                <Hint />
             </div>
             <ButtonContainer>
                 <FormButton>追加</FormButton>
